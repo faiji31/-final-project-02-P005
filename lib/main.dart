183b6d83a838dffcf8a777b98421fec500641bff
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-13 22:14:18 +0600

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 // Import the service
-import 'screens/wrapper.dart';     // We will create this next
+import 'screens/wrapper.dart'; // We will create this next
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,11 +23,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      // --- FIX: Remove the debug banner ---
+      debugShowCheckedModeBanner: false,
+      // -------------------------------------
+
       // Set the home to the Wrapper, which handles the auth state
       home: const Wrapper(),
     );
   }
 }
-
-// Remove the old MyHomePage, as its logic will be split into a 
-// Home Screen (after login) and a Login/Sign up Screen.
